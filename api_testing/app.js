4ef13fd8affdf8cfc59781748ecb00ada719c71d
Leland Wong <lelandw@lelandw-m01.vmware.com>
2017-02-01 17:08:13 -0800

    able to serve static html page using routes

--- api_testing/app.js
+++ api_testing/app.js
@@ -20,10 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api_testing/routes/index.js
+++ api_testing/routes/index.js
@@ -1,9 +1,32 @@
+"use strict"
 var express = require('express');
 var router = express.Router();
+let path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   // res.render('index', { title: 'Express' });
+// });
+
+
+router.get('/', function(req, res, next){
+	let options = {
+		root: path.join(__dirname, "../public/html"),
+		dotfiles: 'deny',
+		headers:{
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+	};
+
+	res.sendFile('index.html', options, function(err){
+		if(err){
+			console.log(err);
+			res.status(err.status).end();
+		} else {
+			console.log('sent index.html');
+		}
+	});
 });
 
 module.exports = router;
